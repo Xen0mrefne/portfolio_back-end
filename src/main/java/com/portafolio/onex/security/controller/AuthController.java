7b33ca29dc536fa2f33f4ea3e9f77841508be734
Xen0mrefne <x3n0g3n1c@gmail.com>
2023-04-03 18:11:18 -0300

    cors fix 10

--- src/main/java/com/portafolio/onex/security/controller/AuthController.java
+++ src/main/java/com/portafolio/onex/security/controller/AuthController.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://angular-portfolio-d72e0.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = "**")
 public class AuthController {
 
     @Autowired
